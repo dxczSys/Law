32461d128ab4bacbb3cdd06370e98b8c943338c9
haodondon <15229396450@163.com>
2019-09-14 14:40:47 +0800

    。。。

--- src/view/fileList.vue
+++ src/view/fileList.vue
@@ -16,6 +16,7 @@
                                 <el-button type="text" v-if="username == 'admin'" class="button" @click="handClickOperate(item, index, 1)">删除</el-button>
                                 <el-button type="text" v-if="username == 'admin'" class="button" @click="handClickOperate(item, index, 2)">修改</el-button>
                                 <el-button type="text" v-if="token" class="button" @click="handClickOperate(item, index, 3)">下载</el-button>
+                                <el-button type="text" v-if="token" class="button" @click="handClickOperate(item, index, 4)">评论</el-button>
                                 <span class="iconfont" v-if="token" :class="{'icondianzan' : !item.isIcon, 'icondianzan1' : item.isIcon }" @click="handClickLike(item, index)"><span>{{item.fdianzan}}</span></span>
                             </div>
                         </div>
@@ -28,6 +29,15 @@
             <el-form :model="form" label-width="100px">
                 <el-form-item label="资源名称">
                     <el-input v-model="form.name"></el-input>
+                </el-form-item>
+
+				<el-form-item label="资源年限">
+                    <el-select v-model="form.year" placeholder="请选择" style="width: 100%;">
+                    	<el-option label="2017" value="2017"> </el-option>
+                    	<el-option label="2018" value="2018"> </el-option>
+                    	<el-option label="2019" value="2019"> </el-option>
+                    	<el-option label="2020" value="2020"> </el-option>
+                    </el-select>
                 </el-form-item>
 
                 <el-form-item label="资源缩略图">
@@ -49,6 +59,27 @@
                 <el-button type="primary" @click="handClickSave">确 定</el-button>
             </div>
         </el-dialog>
+
+        <el-dialog :visible.sync="dialogTableVisible" title="评论">
+            <div class="comment-content">
+                <div v-for="(item, index) in commentData" :key="index">
+                    <div style="line-height:30px;padding-left:20px;">{{item.ctext}}</div>
+                    <div class="dialog-comment-bottom">
+                        <div>{{item.cname}}</div>
+                        <div>{{dateFormat('YYYY-mm-dd', new Date(parseInt(item.cdate)))}}</div>
+                    </div>
+                     <hr style="margin-top: 10px;margin-bottom: 10px;"/>
+                </div>
+                <div>
+                    <el-input v-model="commentVal" type="textarea" :rows="2"></el-input>
+                </div>
+               
+            </div>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogTableVisible = false">取 消</el-button>
+                <el-button type="primary" @click="handClickSaveComment">确 定</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -58,6 +89,10 @@ export default {
         indexN : {
             type : String,
             default : '2'
+        },
+        year : {
+            type : String,
+            default : '2019'
         }
     },
     data() {
@@ -78,7 +113,11 @@ export default {
 
             token : '',
             username : '',
-            isEdit : false
+            isEdit : false,
+
+            dialogTableVisible : false,
+            commentData : [],
+            commentVal : '',
         }
     },
 
@@ -87,8 +126,12 @@ export default {
             handler(n) {
                 if (n != '1') {
                        this.$axios({
-                       	   method :'get',
-                           url : '/Law/fileSel?id=' + n,
+                       	   method :'post',
+                           url : '/Law/fileSel',
+                           data : {
+                           		ftypeid : this.indexN,
+    							fdatetype : this.year
+                           }
                        }).then(res => {
                            if (res.data) {
                                this.dataList = []
@@ -100,6 +143,21 @@ export default {
             deep : true,
             immediate : true
             
+        },
+        year(n){
+            this.$axios({
+                method :'post',
+                url : '/Law/fileSel',
+                data : {
+                    ftypeid : this.indexN,
+                    fdatetype : n
+                }
+            }).then(res => {
+                if (res.data) {
+                    this.dataList = []
+                    this.dataList = res.data
+                }
+            })
         }
     },
 
@@ -113,11 +171,39 @@ export default {
     },
 
     methods : {
+        handClickSaveComment() {
+            this.$axios({
+                method :'post',
+                url : '/Law/commentAdd',
+                data : { 
+                    ctext: this.commentVal,
+                    onlyid: this.fid}
+            }).then(res => {
+                if (res.data.code) {
+                     this.$axios({
+                        method :'get',
+                        url : '/Law/commentSel?id=' + this.fid
+                    }).then(res => {
+                        if (res.data.code) {
+                            this.commentData = []
+                            this.commentData = res.data.msg
+                        }
+                    })
+                }
+            })
+        },
     	
     	getList() {
+    		
+    		let _seldata = {
+    			ftypeid : this.indexN,
+    			fdatetype : this.year
+    		}
+    		
     		this.$axios({
-                       	   method :'get',
-                           url : '/Law/fileSel?id=' + this.indexN,
+                       	   method :'post',
+                           url : '/Law/fileSel',
+                           data : _seldata
                        }).then(res => {
                            if (res.data) {
                                this.dataList = []
@@ -185,7 +271,8 @@ export default {
                    	  fname : this.form.name,
                    	  fpath : this.imageUrlParam,
                    	  ftypeid : this.indexN,
-                   	  fpathone : this.fileParam
+                   	  fpathone : this.fileParam,
+                   	  fdatetype : this.form.year
                    	  
                    }
                }).then(res => {
@@ -203,8 +290,8 @@ export default {
                    	  fname : this.form.name,
                    	  fpath : this.imageUrlParam,
                    	  ftypeid : this.indexN,
-                   	  fpathone : this.fileParam
-                   	  
+                   	  fpathone : this.fileParam,
+                   	  fdatetype : this.form.year
                    }
                }).then(res => {
                    if (res.data.code) {
@@ -233,12 +320,25 @@ export default {
                 //修改
                 this.fid = item.fid
                 this.form.name = item.fname
+                this.form.year = item.fdatetype
                 this.imageUrl = this.baseIp + item.fpath
                 this.imageUrlParam = item.fpath
                 this.fileParam = item.fpathone
                 this.fileList = [{ name : item.fpathone, url : item.fpathone }]
                 this.dialogVisible = true
                 this.isEdit = true
+            }else if (num == 4) {
+                this.fid = item.fid
+                this.dialogTableVisible = true
+                this.$axios({
+                    method :'get',
+                    url : '/Law/commentSel?id=' + item.fid
+                }).then(res => {
+                    if (res.data.code) {
+                        this.commentData = []
+                        this.commentData = res.data.msg
+                    }
+                })
             }else {
                 //下载
                 
@@ -322,6 +422,13 @@ export default {
 }
 .time{
     color: #888;
+}
+.dialog-comment-bottom{
+    display: flex;
+    justify-content: space-between;
+    padding-left: 10px;
+    line-height: 36px;
+    color: red;
 }
 </style>
 
--- src/view/home.vue
+++ src/view/home.vue
@@ -1,28 +1,54 @@
 <template>
     <div class="wrapper">
         <div class="nav-wrapper">
-            <el-menu :default-active="index" class="el-menu-vertical-demo"  background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+            <el-menu :unique-opened="true" :default-active="index" class="el-menu-vertical-demo"  background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
                 <el-menu-item index="1" @click="index = '1'" v-if="username == 'admin'">
                     <i class="iconfont icontouxiang"></i>
                     <span slot="title">用户管理</span>
                 </el-menu-item>
-                <el-menu-item index="2" @click="index = '2'">
-                    <i class="iconfont iconwenxianbianmu"></i>
-                    <span slot="title">文献</span>
-                </el-menu-item>
-                <el-menu-item index="3" @click="index = '3'">
-                    <i class="iconfont iconshipin"></i>
-                    <span slot="title">视频</span>
-                </el-menu-item>
-                <el-menu-item index="4" @click="index = '4'">
-                    <i class="iconfont icontuwenguanli"></i>
-                    <span slot="title">图文</span>
-                </el-menu-item>
+                 <el-submenu index="2" @click.native="index = '2'">
+                    <template slot="title">
+                        <i class="iconfont iconwenxianbianmu"></i>
+                        <span slot="title">文献</span>
+                    </template>
+                    <el-menu-item-group>
+                        <el-menu-item index="1-1" @click="year='2017'">2017</el-menu-item>
+                        <el-menu-item index="1-2" @click="year='2018'">2018</el-menu-item>
+                        <el-menu-item index="1-3" @click="year='2019'">2019</el-menu-item>
+                        <el-menu-item index="1-4" @click="year='2020'">2020</el-menu-item>
+                    </el-menu-item-group>
+                </el-submenu>
+
+                <el-submenu index="3" @click.native="index = '3'">
+                    <template slot="title">
+                        <i class="iconfont iconshipin"></i>
+                        <span slot="title">视频</span>
+                    </template>
+                    <el-menu-item-group>
+                        <el-menu-item index="1-1" @click="year='2017'">2017</el-menu-item>
+                        <el-menu-item index="1-2" @click="year='2018'">2018</el-menu-item>
+                        <el-menu-item index="1-3" @click="year='2019'">2019</el-menu-item>
+                        <el-menu-item index="1-4" @click="year='2020'">2020</el-menu-item>
+                    </el-menu-item-group>
+                </el-submenu>
+
+                <el-submenu index="4" @click.native="index = '4'">
+                     <template slot="title">
+                        <i class="iconfont icontuwenguanli"></i>
+                        <span slot="title">图文</span>
+                    </template>
+                    <el-menu-item-group>
+                        <el-menu-item index="1-1" @click="year='2017'">2017</el-menu-item>
+                        <el-menu-item index="1-2" @click="year='2018'">2018</el-menu-item>
+                        <el-menu-item index="1-3" @click="year='2019'">2019</el-menu-item>
+                        <el-menu-item index="1-4" @click="year='2020'">2020</el-menu-item>
+                    </el-menu-item-group>
+                </el-submenu>
             </el-menu>
         </div>
         <div class="home-content">
             <userTable v-if="index == '1'"></userTable>
-            <fileList v-else :indexN="index"></fileList>
+            <fileList v-else :indexN="index" :year="year"></fileList>
         </div>
     </div>
 </template>
@@ -37,6 +63,7 @@ export default {
         return {
             index : '2',
             username : '',
+            year : '2019'
         }
     },
     mounted() {
